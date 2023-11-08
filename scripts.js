d81b2c079a7b5cfb1011f47defb6f86dfdfde015
Andrey Chaus <chausandrei15@gmail.com>
2023-11-08 11:14:58 +0200

    Додан код у файл scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 function showMessage() {
-  alert('Hello World!');
+  alert('Hello World!'); // Функція showMessage() викликає функцію alert() з повідомленням 'Hello World!'
+
 }
-window.addEventListener('load', showMessage);
+window.addEventListener('load', showMessage); // Відправити подію "load" на вікно, коли сторінка завантажена
