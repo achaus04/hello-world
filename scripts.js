e584e2826900344f477d81a10e9b5641c05b626f
Andrey Chaus <chausandrei15@gmail.com>
2023-11-15 09:16:00 +0200

    Модифікован код у scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-function showMessage() {
-  alert('Hello World!'); // Функція showMessage() викликає функцію alert() з повідомленням 'Hello World!'
-
+function showMessage() { // Оголошення функції showMessage
+  let userName = prompt("Будь ласка, введіть ваше ім'я:"); // Створення змінної userName, яка отримує значення з prompt
+  if (userName != null && userName !== "") { // Перевірка, чи введене ім'я не є null і не є пустим рядком
+    alert('Hello, ' + userName + '!'); // Відображення повідомлення з ім'ям користувача
+  }
 }
 window.addEventListener('load', showMessage); // Відправити подію "load" на вікно, коли сторінка завантажена
